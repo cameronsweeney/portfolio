2fa6087159b2db6c542022cdc09848907374c8bd
Cameron Sweeney <camswee@gmail.com>
2022-03-04 23:29:25 -0500

    bringing back the old style.css, double-checking urls.py and views.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import HomePageView, AboutPageView, ProjectsPageView, CodingPageView, ContactPageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('about', AboutPageView.as_view(), name='about'),
-    path('projects', ProjectsPageView.as_view(), name='projects'),
-    path('coding', CodingPageView.as_view(), name='coding'),
-    path('contact', ContactPageView.as_view(), name='contact'),
+    path('', HomePageView.as_view()),
+    path('home/', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
+    path('projects/', ProjectsPageView.as_view(), name='projects'),
+    path('coding/', CodingPageView.as_view(), name='coding'),
+    path('contact/', ContactPageView.as_view(), name='contact'),
 ]
